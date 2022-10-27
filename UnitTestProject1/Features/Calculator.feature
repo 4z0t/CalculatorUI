aff234edf3cc863df7364880cf5390a670df141e
4z0t <gastec139@gmail.com>
2022-10-27 20:37:38 +0300

    SpecFlow tests

--- UnitTestProject1/Features/Calculator.feature
+++ UnitTestProject1/Features/Calculator.feature
@@ -2,9 +2,16 @@
 	Simple calculator for adding two numbers
 
 @positive
-Scenario: Add two numbers
+Scenario: Do number action
 	Given Calculator is initialized
-	When I type number 3.0
-	And I press Plus
-	And I type number 4.0
-	Then the result should be 7.0 on the screen
+	When I type number <firstValue>
+	And I press <action>
+	And I type number <secondValue>
+	Then the result should be <result> on the screen
+
+Examples: 
+| firstValue | secondValue  | action   | result   |
+| 1          | 2            | Plus     | 3        |
+| 2          | 3            | Minus    | -1       |
+| 2          | 2            | Multiply | 4        |
+| 2          | 2            | Divide   | 1        |
--- UnitTestProject1/UnitTestSpecFlow.cs
+++ UnitTestProject1/UnitTestSpecFlow.cs
@@ -9,14 +9,39 @@ using Calculator;
 using SpecFlow.Internal;
 using SpecFlow;
 using TechTalk.SpecFlow;
+using TechTalk.SpecFlow.Assist;
 
 namespace UnitTestProject1
 {
-    [Binding]
 
-    class UnitTestSpecFlow
+
+
+    [Binding, Scope(Feature = "Calculator")]
+
+    class SpecFlowTest
     {
-        
+        public string CharToName(char c)
+        {
+            switch (c)
+            {
+
+                case '0': return "Zero";
+                case '1': return "One";
+                case '2': return "Two";
+                case '3': return "Three";
+                case '4': return "Four";
+                case '5': return "Five";
+                case '6': return "Six";
+                case '7': return "Seven";
+                case '8': return "Eight";
+                case '9': return "Nine";
+                case '.': return "Dot";
+                case ',': return "Dot";
+                default:
+                    throw new ArgumentException("Unknown char");
+            }
+        }
+
         MainWindow window;
 
         [BeforeScenario("calculator")]
@@ -27,7 +52,7 @@ namespace UnitTestProject1
 
         }
 
-        [When("I press (.*)")]
+        [When(@"I press (.*)")]
         public void Press(string name)
         {
             var btn = (Button)window.FindName(name);
@@ -35,10 +60,23 @@ namespace UnitTestProject1
 
         }
 
-        [Then("the result should be (.*) on the screen")]
-        public void Result(string expected)
+
+        [When(@"I type number (.*)")]
+        public void TypeNumber(double number)
         {
-            Assert.AreEqual(expected, ((Label)window.FindName("Equation")).Content.ToString());
+            foreach (char c in number.ToString())
+            {
+                var btn = (Button)window.FindName(CharToName(c));
+                btn.RaiseEvent(new RoutedEventArgs(Button.ClickEvent));
+            }
         }
+
+        [Then("the result should be (.*) on the screen")]
+        public void Result(double expected)
+        {
+            Assert.AreEqual(expected.ToString(), ((Label)window.FindName("Display")).Content.ToString());
+        }
+
+
     }
 }
